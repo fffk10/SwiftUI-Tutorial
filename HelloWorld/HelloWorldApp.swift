e3f367e69417acb134945ee82ebbdd59c73338f7
fffk10 <warafffk10@gmail.com>
2021-08-02 01:47:09 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by FFFK10 on 2021/08/02.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
